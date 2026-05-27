6288323aa47ea0dde684a34848371f6fd2bf80b0
Алексеев Илья <iaalekseev45@gmail.com>
2026-05-27 22:49:50 +0500

    revert

---
+++ blogicum/blog/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.models import User
+
+from .models import Comment, Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'text', 'pub_date', 'location', 'category', 'image')
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('text',)
+        
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email')
